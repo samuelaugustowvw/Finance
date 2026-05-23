e2622a25eacf1375edbe6e0f1176c210b09a30aa
samuel <mestredascontas13@gmail.com>
2026-05-23 09:14:50 -0300

    feat: add vercel json

--- finance-frontend/src/lib/api.ts
+++ finance-frontend/src/lib/api.ts
@@ -1,11 +1,15 @@
 import axios from 'axios'
 
+const isProd = import.meta.env.PROD
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: isProd
+    ? 'https://finance-production-e7a5.up.railway.app'
+    : 'http://localhost:3333',
 })
 
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('finance:token')
-    if (token) config.headers.Authorization = `Bearer ${token}`
-    return config
+  const token = localStorage.getItem('finance:token')
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
 })
